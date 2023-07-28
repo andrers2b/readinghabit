d8a4d920646fd087bfc0721c458d4ad1abec2930
Andre <andre@Andre-MacBook-Air.local>
2023-07-28 15:59:34 +0800

    Cronning

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -4,16 +4,13 @@ import type { NextRequest } from 'next/server';
 // handle 404
 export function GET(request: NextRequest) {
 
-    //lo
+    console.log("Cron running")
 
 
 
   return NextResponse.json(
     {
-      body: request.body,
-      path: request.nextUrl.pathname,
-      query: request.nextUrl.search,
-      cookies: request.cookies.getAll(),
+      
     },
     {
       status: 200,

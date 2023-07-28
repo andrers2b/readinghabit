57cd8d2fe84f3ee3d4181d8a165d6a87ef13ddac
Andre <andre@Andre-MacBook-Air.local>
2023-07-28 16:23:12 +0800

    logging

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -10,7 +10,10 @@ export function GET(request: NextRequest) {
 
   return NextResponse.json(
     {
-      
+      body: request.body,
+      path: request.nextUrl.pathname,
+      query: request.nextUrl.search,
+      cookies: request.cookies.getAll(),
     },
     {
       status: 200,
--- src/app/api/serverless-example/route.ts
+++ src/app/api/serverless-example/route.ts
@@ -5,7 +5,7 @@ import type { NextRequest } from 'next/server';
 export function GET(request: NextRequest) {
 
     //lo
-
+    console.log("Serverless running")
 
 
   return NextResponse.json(
